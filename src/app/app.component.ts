b107e8facc45cc83e8e136534a5951f1c0e81983
rivera_s <rivera_s@ca01.ub.lan>
2026-02-16 14:30:22 +0800

    feature update app title with our github greeting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'my-first-repo-app';
+  title = 'Hello Github, this is my first Angular push';
 }
